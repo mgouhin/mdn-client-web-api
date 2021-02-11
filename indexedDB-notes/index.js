b1e0557eacd6da29c4b75c8c93304a2a3701dbaa
mike4850 <mgouhin@gmail.com>
2021-02-10 16:26:03 -0800

    Basic IndexedDB example setup

---
+++ indexedDB-notes/index.js
@@ -0,0 +1,53 @@
+// Create needed constants
+const list = document.querySelector('ul');
+const titleInput = document.querySelector('#title');
+const bodyInput = document.querySelector('#body');
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('form button');
+
+// Create an instance of a db object for us to store the opened database in
+let db;
+
+// Ensures script doesn't fail if db is still loading
+window.onload = function () {
+  // Open our database; it is created if it doesn't already exist
+  // (see onupgradeneeded below)
+  let request = window.indexedDB.open('notes_db', 1);
+
+  // onerror handler signifies that the database didn't open successfully
+  request.onerror = function () {
+    console.log('Database failed to open');
+  };
+
+  // onsuccess handler signifies that the database opened successfully
+  request.onsuccess = function () {
+    console.log('Database opened successfully');
+
+    // Store the opened database object in the db variable.
+    db = request.result;
+
+    // Run the displayData() function to display the notes already in the IDB
+    displayData();
+  };
+
+  // Setup the database tables if this has not already been done
+  request.onupgradeneeded = function (e) {
+    // Grab a reference to the opened database
+    let db = e.target.result;
+
+    // Create an objectStore to store our notes in (basically 
+    // like a single table)
+    // including an auto-incrementing key
+    let objectStore = db.createObjectStore('notes_os', {
+      keyPath: 'id',
+      autoIncrement: true
+    });
+
+    // Define what data items the objectStore will contain
+    objectStore.createIndex('title', 'title', { unique: false });
+    objectStore.createIndex('body', 'body', { unique: false });
+
+    console.log('Database setup complete');
+  };
+
+};

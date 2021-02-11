03db9598590f2bc4f77a56505e86d666ca636ff8
mike4850 <mgouhin@gmail.com>
2021-02-10 17:04:02 -0800

    Continued js updates to indexDB example

--- indexedDB-notes/index.js
+++ indexedDB-notes/index.js
@@ -50,4 +50,118 @@ window.onload = function () {
     console.log('Database setup complete');
   };
 
+  // Create an onsubmit handler so that when the form is submitted
+  // the addData() function is run
+  form.onsubmit = addData;
+
+  // Define the addData() function
+  function addData(e) {
+    // prevent default - we don't want the form to submit in the
+    // conventional way
+    e.preventDefault();
+
+    // grab the values entered into the forrm fields and store
+    // them in an object ready for being inserted into the DB
+    let newItem = {
+      title: titleInput.value,
+      body: bodyInput.value;
+    };
+
+    // open a read/write db transaction, ready for adding the data
+    let transaction = db.transaction(['notes_os'], 'readwrite');
+
+    // call an object store that's already been addded to the database
+    let objectStore = transaction.objectStore('notes_os');
+
+    // Make a request to add our newItem object to the object store
+    let request = objectStore.add(newItem);
+    request.onsuccess = function () {
+      // Clear the form, ready for the next entry
+      titleInput.value = '';
+      bodyInput.value = '';
+    };
+
+    // Report on the success of the transaction completing, when 
+    // everything is done
+    transaction.oncomplete = function () {
+      console.log('Transaction completed: database modification finished');
+
+      // update the display of data to show the newly added item,
+      // by running displayData() again.
+      displayData();
+    };
+
+    transaction.onerror = function () {
+      console.log('Transaction not opened due to error');
+    };
+  }
+
+  // Define the displayData() function
+  function displayData() {
+    // Here we empty the contents of the list element each time
+    // the display is updated
+    // If you didn't do this, you'd get duplicates listed each
+    // time a new note is added
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+
+    // Open our object store and then get a cursor - which iterates
+    // through all the different data items in the store
+    let objectStore = db.transaction('notes_os').objectStore('notes_os');
+    objectStore.openCursor().onsuccess = function (e) {
+      // Get a reference to the cursor
+      let cursor = e.target.result;
+
+      // If there is still another data item to iterate through,
+      // keep running this code
+      if (cursor) {
+        // Create a list item, h3, and p to put each data item 
+        // inside when displaying it
+        // structure the HTML fragment, and append it inside the list
+        const listItem = document.createElement('li');
+        const h3 = document.createElement('h3');
+        const para = document.createElement('p');
+
+        listItem.appendChild(h3);
+        listItem.appendChild(para);
+        list.appendChild(listItem);
+
+        // Put the data from the cursor inside the h3 and para
+        h3.textContent = cursor.value.title;
+        para.textContent = cursor.value.body;
+
+        // Store the ID of the data item inside an attribute on the list
+        // item, so we know which item itt corresponds to. This will
+        // be useful later when we want to delete items.
+        listItem.setAttribute('data-note-id', cursor.value.id);
+
+        // Create a button and place it inside each listItem
+        const deleteBtn = document.createElement('button');
+        listItem.appendChild(deleteBtn);
+        deleteBtn.textContent = 'Delete';
+
+        // Set an event handler so that when the button is clicked,
+        // the deleteItem() function is run
+        deleteBtn.onclick = deleteItem;
+
+        // Iterate to the next item in the cursor
+        cursor.continue();
+      } else {
+        // Again, if list item is empty, display a 'No notes stored' message
+        if (!list.firstChild) {
+          const listItem = document.createElement('li');
+          listItem.textContent = 'No notes stored.';
+          list.appendChild(listItem);
+        }
+        // if there are no more cursor items to iterate through, say so
+        console.log('Notes all displayed');
+      }
+    }
+  }
+
+  // Define the deleteItem() function
+  function deleteItem(e) {
+    // fill out
+  }
 };
